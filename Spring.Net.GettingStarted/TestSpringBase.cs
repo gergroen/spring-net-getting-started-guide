8a0e9fb5ba16a828ebbe3154d2157b4f858fb552
gergroen <gergroen@gmail.com>
2016-09-30 22:40:59 +0200

    Added first spring tests

---
+++ Spring.Net.GettingStarted/TestSpringBase.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using Spring.Context;
+using Spring.Context.Support;
+
+namespace Spring.Net.GettingStarted
+{
+    [TestFixture]
+    public abstract class TestSpringBase<TConfiguration>
+    {
+        protected IApplicationContext ApplicationContext;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var codeConfigApplicationContext = new CodeConfigApplicationContext();
+            codeConfigApplicationContext.Scan(a =>
+            {
+                return typeof(TConfiguration).Assembly == a;
+            }, t =>
+            {
+                return typeof(TConfiguration) == t;
+            });
+            codeConfigApplicationContext.Refresh();
+            ApplicationContext = codeConfigApplicationContext;
+        }
+    }
+}
